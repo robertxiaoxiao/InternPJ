cc52d6110bd11f4e4a7857d14f0a0e8e7d9a4bda
robertxiaoxiao <276408734@qq.com>
2019-07-29 22:13:23 +0800

    chunkstoremds

---
+++ ChunkStoreMDS.cpp
@@ -0,0 +1,41 @@
+/*
+ ChunkStoreMDS
+
+ maintain a file set
+
+*/
+
+#include "FileFolder.cpp"
+
+
+// basic fileInfo  as one FileSet-table row
+struct fileInfo{
+
+     // filename unique
+     string filepath ;
+
+    //  check free
+    bool  Beusing ;
+
+    // help to recover client's filelist info 
+    string owner ;
+
+
+};
+class ChunkStoreMDS{
+
+        private :
+
+
+
+
+
+        public :
+
+
+
+
+
+
+
+}；
